a7bd6cf3be5d95bed31e0c5b0bf66d8df9a549b1
DouglasJr12 <douglasjuniorinfo@gmail.com>
2026-09-20 20:46:55 -0300

    docs: adiciona documentação dos endpoints com Swagger

--- src/main/java/com/dog/starwars_api/controller/WorldController.java
+++ src/main/java/com/dog/starwars_api/controller/WorldController.java
@@ -7,15 +7,22 @@ import com.dog.starwars_api.dto.WorldResponse;
 import com.dog.starwars_api.mapper.WorldMapper;
 import com.dog.starwars_api.service.SwapInfoApi;
 import com.dog.starwars_api.service.WorldService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+
+
+
+
 
 @RestController
 @RequestMapping("/api/mundos")
+@Tag(name = "Mundos",
+        description = "Operações para gerenciamento de mundos")
 public class WorldController {
     private final WorldMapper mapper;
     private final WorldService service;
@@ -28,30 +35,50 @@ public class WorldController {
         this.swapInfoApi = swapInfoApi;
     }
 
+    @Operation(
+            summary = "Cadastrar mundo",
+            description = "Cadastra um novo mundo no banco de dados local."
+    )
     @PostMapping
     public ResponseEntity<WorldResponse> createWorld(@Valid @RequestBody WorldRequest request) {
         WorldResponse response = mapper.toResponse(service.createWorld(request));
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(
+            summary = "Consultar mundos locais",
+            description = "Retorna todos os mundos cadastrados localmente"
+    )
     @GetMapping("/local")
     public ResponseEntity<List<WorldResponse>> findAllWorldLocal(){
             List<WorldResponse> responseList = mapper.toWorldsResponseList(service.findAllWorldsLocal()) ;
         return ResponseEntity.ok(responseList);
     }
 
+    @Operation(
+            summary = "Consultar mundo na SWAPI",
+            description = "Consulta um mundo na API externa SWAPI utilizando seu ID."
+    )
     @GetMapping("/externo/{id}")
     public ResponseEntity<SwapInfoDto> getSwapInfoWorlds(@PathVariable Long id){
         SwapInfoDto swapInfoDto = swapInfoApi.getWorldsApi(id);
          return ResponseEntity.ok(swapInfoDto);
     }
 
+    @Operation(
+            summary = "Atualizar um mundo",
+            description = "Atualiza os dados de um mundo existente."
+    )
     @PutMapping("/{id}")
     public ResponseEntity<WorldResponse> updateWorld(@PathVariable Long id,@RequestBody WorldRequest request){
         WorldResponse response = mapper.toResponse(service.updateWorld(id,request));
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Operation(
+            summary = "Excluir mundo",
+            description = "Exclui um mundo cadastrado localmente utilizando seu ID."
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteWorld(@PathVariable Long id){
             service.deleteWorld(id);
